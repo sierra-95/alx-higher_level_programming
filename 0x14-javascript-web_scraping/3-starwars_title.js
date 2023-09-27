b356317c02888b2c6bdb5a5494f241b22f3691ff
Michael Machohi <111045570+sierra-95@users.noreply.github.com>
2023-09-27 15:43:51 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // status of a request
 const myRequest = require('request');
-const myUrl = 'http://swapi.co/api/films/' + process.argv[2];
+const myUrl = 'https://swapi-api.alx-tools.com/api/films/:id' + process.argv[2];
 myRequest(myUrl, function (err, res, body) {
   if (err) {
     console.log(err);
